e04960f17ccbaeec2d1a989c7a760fbc6471ae28
Chris <85950629+chriseverts@users.noreply.github.com>
2021-10-20 15:05:21 -0500

    Delete env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-host = "157.230.209.171"
-username = "germain_1467"
-password = "8A486YgW1XMLedoP5mgqBlgw0L3NKoNC"
-
-def get_db_url(username,password,host,db_name):
-    return f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
